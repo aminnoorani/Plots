38fde1077b2c198d3971b0246b9e45a9d91d390a
Aminnn <45661298+aminnoorani@users.noreply.github.com>
2022-12-12 13:06:04 -0500

    Update DotPlot.R

--- DotPlot.R
+++ DotPlot.R
@@ -11,8 +11,8 @@ markers <- melt(markers)
 
 markers2 <- markers$name %>% unique()
 colnames(markers) <- c("name","variable","Gene_Score")
-
-markers %>% sample_n(5)
+#Sorting Clusters
+markers$variable <- factor(markers$variable,levels = c("C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13","C14","C15","C16","C17","C18","C19","C20","C21","C22","C23","C24","C25","C26","C27","C28","C29","C30","C31","C32","C33","C34","C35","C36","C37","C38","C39","C40","C41"))
 
 #Simple Bubble
 markers %>% filter(name %in% markers2) %>% 
